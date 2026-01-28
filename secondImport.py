2b27f13ed2ec5f8031dc780f7e93408e9e6e0774
Aditya Srikanth <as140@illinois.edu>
2026-01-28 15:52:23 -0600

    gonna do it from within paraview instead of exporting csv. will be back later.

--- secondImport.py
+++ secondImport.py
@@ -7,6 +7,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+from paraview import servermanager
+from vtk.util import numpy_support as ns
+import numpy as np
+
+
+
 # this stuff gets current working directory
 base = Path.cwd()
 
@@ -61,52 +67,74 @@ plotOverLine1.Source.Point2 = [0.6, 0, 0]
 plotOverLine1.Source.Resolution = 5000
 plotOverLine1Display = Show(plotOverLine1, renderView1)
 
-# spreadsheet view to export points on line as csv
-CreateLayout('Layout #2')
-SetActiveView(None)
-spreadSheetView1 = CreateView('SpreadSheetView')
-spreadSheetView1.ColumnToSort = ''
-spreadSheetView1.BlockSize = 1024
-plotOverLine1Display_2 = Show(plotOverLine1, spreadSheetView1)
-layout2 = GetLayoutByName("Layout #2")
-AssignViewToLayout(view=spreadSheetView1, layout=layout2, hint=0)
-ExportView(str(base) + '/something.csv', view=spreadSheetView1)
-
-# reads csv and drops nan columns
-csvImport = pd.read_csv("something.csv")
-csvForEdit1 = csvImport.dropna()
-csvForEdit2 = csvForEdit1.reset_index(drop=True)
-csvForEdit2.to_csv('droppednan.csv', index=False)
 
 
-x = csvForEdit2["Points_0"]
-y = csvForEdit2["VectorGradient_4"]
+data = servermanager.Fetch(plotOverLine1)
+pd = data.GetPointData()
+print([pd.GetArrayName(i) for i in range(pd.GetNumberOfArrays())])
+
+
+pts_vtk = data.GetPoints()
+
+print("VTK points object:", pts_vtk)
+print("Number of points:", pts_vtk.GetNumberOfPoints())
+
+
+arr = pd.GetArray("VectorGradient")
+print("VectorGradient components:", arr.GetNumberOfComponents())
+print("VectorGradient tuples:", arr.GetNumberOfTuples())
+
+vg = ns.vtk_to_numpy(arr)
+print("numpy shape:", vg.shape)   # (N, ncomp)
+
+
+
+# # spreadsheet view to export points on line as csv
+# CreateLayout('Layout #2')
+# SetActiveView(None)
+# spreadSheetView1 = CreateView('SpreadSheetView')
+# spreadSheetView1.ColumnToSort = ''
+# spreadSheetView1.BlockSize = 1024
+# plotOverLine1Display_2 = Show(plotOverLine1, spreadSheetView1)
+# layout2 = GetLayoutByName("Layout #2")
+# AssignViewToLayout(view=spreadSheetView1, layout=layout2, hint=0)
+# ExportView(str(base) + '/something.csv', view=spreadSheetView1)
+
+# # reads csv and drops nan columns
+# csvImport = pd.read_csv("something.csv")
+# csvForEdit1 = csvImport.dropna()
+# csvForEdit2 = csvForEdit1.reset_index(drop=True)
+# csvForEdit2.to_csv('droppednan.csv', index=False)
+
+
+# x = csvForEdit2["Points_0"]
+# y = csvForEdit2["VectorGradient_4"]
+
+# # plt.figure()
+# # plt.plot(x, y)
+# # plt.xlabel("x_direction")
+# # plt.ylabel("dv/dy")
+# # plt.show()
+
+# x_np = np.array(x)
+# y_np = np.array(y)
+# final_grad = np.diff(y_np)/np.diff(x_np)
+# x_np = np.delete(x_np, 0)
+# # final_grad = np.gradient(y_np, x_np)
 
 # plt.figure()
-# plt.plot(x, y)
+# plt.plot(x_np, final_grad)
 # plt.xlabel("x_direction")
-# plt.ylabel("dv/dy")
+# plt.ylabel("dv2/dydx")
 # plt.show()
 
-x_np = np.array(x)
-y_np = np.array(y)
-final_grad = np.diff(y_np)/np.diff(x_np)
-x_np = np.delete(x_np, 0)
-# final_grad = np.gradient(y_np, x_np)
-
-plt.figure()
-plt.plot(x_np, final_grad)
-plt.xlabel("x_direction")
-plt.ylabel("dv2/dydx")
-plt.show()
 
 
 
-
-# plt.figure()
-# plt.plot(x, y, label="dv/dy")
-# plt.plot(x_np, final_grad, label="d/dx(dv/dy)")
-# plt.xlabel("x_direction")
-# plt.legend()
-# plt.show()
+# # plt.figure()
+# # plt.plot(x, y, label="dv/dy")
+# # plt.plot(x_np, final_grad, label="d/dx(dv/dy)")
+# # plt.xlabel("x_direction")
+# # plt.legend()
+# # plt.show()
 
